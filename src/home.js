06861ed6ca6e3f30c1960d30526e5653653abfd6
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-04-25 11:39:37 -0500

    home page function and export

--- src/home.js
+++ src/home.js
@@ -1,3 +1,9 @@
+function homeText(text) {
+  const homeInfo = document.createElement('p');
+  homeInfo.textContent = text;
+  return homeInfo;
+}
+
 function homePage() {
   const home = document.createElement('div');
   home.classList.add('home');
@@ -5,12 +11,12 @@ function homePage() {
   const homeTitle = document.createElement('h2');
   homeTitle.textContent = 'Welcome to Le Véritable,';
 
-  const homeInfo = document.createElement('p');
-  homeInfo.textContent =
-    ' Where every dish is crafted with the finest ingredients and the utmost care. Our culinary experts have carefully curated a menu that combines French tradition with contemporary flair, resulting in an unforgettable dining experience that is truly worthy of the most discerning palate. Our goal is to delight and amaze you with every bite, and we look forward to serving you soon.';
-
   home.appendChild(homeTitle);
-  home.appendChild(homeInfo);
+  home.appendChild(
+    homeText(
+      ' where every dish is crafted with the finest ingredients and the utmost care. Our culinary experts have carefully curated a menu that combines French tradition with contemporary flair, resulting in an unforgettable dining experience that is truly worthy of the most discerning palate. Our goal is to delight and amaze you with every bite, and we look forward to serving you soon.'
+    )
+  );
 
   return home;
 }
