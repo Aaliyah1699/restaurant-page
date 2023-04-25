601862426924e706af80abc35391fa83235f7467
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-04-25 13:31:06 -0500

    refactor to use func for larger text and add class

--- main.js
+++ main.js
@@ -12,35 +12,47 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+function contactInfoAbout(text) {
+  const contactAboutInfo = document.createElement('p');
+  contactAboutInfo.classList.add('contactInfo');
+  contactAboutInfo.textContent = text;
+  return contactAboutInfo;
+}
+
 function contactPage() {
   const contact = document.createElement('div');
   contact.classList.add('contact');
 
   const contactTitle = document.createElement('h2');
+  contactTitle.classList.add('contactTitle');
   contactTitle.textContent = 'Contact Us';
 
   const contactInfo = document.createElement('p');
+  contactInfo.classList.add('contactInfo');
   contactInfo.textContent = 'Call to make reservations';
 
   const contactNumber = document.createElement('p');
+  contactNumber.classList.add('contactInfo');
   contactNumber.textContent = 'Phone: 555-555-5555';
 
   const contactEmail = document.createElement('p');
+  contactEmail.classList.add('contactInfo');
   contactEmail.textContent = 'reservations@leveritable.com';
 
   const contactAbout = document.createElement('p');
+  contactAbout.classList.add('contactInfo');
   contactAbout.textContent = 'About Us';
-
-  const contactAboutInfo = document.createElement('p');
-  contactAboutInfo.textContent =
-    "At Le Véritable, we believe that dining is not just about the food, but the entire experience. From the moment you step into our elegant restaurant, you'll be transported to a world of refinement and sophistication. Our chefs are dedicated to creating dishes that not only taste incredible but are also beautifully presented, making every meal a feast for the eyes as well as the taste buds. We are committed to providing our guests with the highest level of service and attention to detail, ensuring that every visit to Le Véritable is an unforgettable one.";
 
   contact.appendChild(contactTitle);
   contact.appendChild(contactInfo);
   contact.appendChild(contactNumber);
   contact.appendChild(contactEmail);
   contact.appendChild(contactAbout);
-  contact.appendChild(contactAboutInfo);
+  contact.appendChild(
+    contactInfoAbout(
+      "At Le Véritable, we believe that dining is not just about the food, but the entire experience. From the moment you step into our elegant restaurant, you'll be transported to a world of refinement and sophistication. Our chefs are dedicated to creating dishes that not only taste incredible but are also beautifully presented, making every meal a feast for the eyes as well as the taste buds. We are committed to providing our guests with the highest level of service and attention to detail, ensuring that every visit to Le Véritable is an unforgettable one."
+    )
+  );
 
   return contact;
 }
