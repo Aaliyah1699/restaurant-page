ae83c064becfbd75f1b9047ab7c051515410cacc
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-04-24 21:33:13 -0500

    dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  devtool: false,
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
