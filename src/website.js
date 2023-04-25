de6d8afd935a2d8e6e1a70456c37f3d07ae071d6
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-04-25 12:15:29 -0500

    header, nav, buttons, footer, and export

--- src/website.js
+++ src/website.js
@@ -2,3 +2,92 @@ import loadHome from './home';
 import loadMenu from './menu';
 import loadContact from './contact';
 
+function header() {
+  const headerSection = document.createElement('header');
+  const title = document.createElement('h1');
+  title.textContent = 'Le Véritable';
+  headerSection.appendChild(title);
+  return headerSection;
+}
+
+function activateButtons() {
+  const buttons = document.querySelectorAll('.nav-button');
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      buttons.forEach((button) => {
+        button.classList.remove('active');
+      });
+      button.classList.add('active');
+    });
+  });
+}
+
+function nav() {
+  const navSection = document.createElement('nav');
+  const homeButton = document.createElement('button');
+  homeButton.classList.add('nav-button');
+  homeButton.textContent = 'Home';
+  homeButton.addEventListener('click', () => {
+    activateButtons(homeButton);
+    loadHome();
+  });
+  const menuButton = document.createElement('button');
+  menuButton.classList.add('nav-button');
+  menuButton.textContent = 'Menu';
+  menuButton.addEventListener('click', () => {
+    activateButtons(menuButton);
+    loadMenu();
+  });
+  const contactButton = document.createElement('button');
+  contactButton.classList.add('nav-button');
+  contactButton.textContent = 'Contact';
+  contactButton.addEventListener('click', () => {
+    activateButtons(contactButton);
+    loadContact();
+  });
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+  return navSection;
+}
+
+function createMain() {
+  const main = document.createElement('div');
+  main.classList.add('main');
+  main.setAttribute('id', 'main-content');
+  return main;
+}
+
+function createFooter() {
+  const footer = document.createElement('footer');
+  footer.classList.add('footer');
+
+  const footerText = document.createElement('p');
+  footerText.textContent = 'Aaliyah Montgomery';
+
+  const imageCredit = document.createElement('p');
+  imageCredit.textContent = 'Photo by Rachel Claire';
+
+  const creditLink = document.createElement('a');
+  creditLink.href =
+    'https://www.pexels.com/photo/row-of-stools-next-to-long-table-covered-with-tablecloth-in-dark-hall-4577179/';
+
+  creditLink.appendChild(imageCredit);
+  footer.appendChild(creditLink);
+  footer.appendChild(footerText);
+
+  return footer;
+}
+
+function loadWebsite() {
+  const content = document.getElementById('content');
+  content.appendChild(header());
+  content.appendChild(nav());
+  content.appendChild(createMain());
+  content.appendChild(createFooter());
+
+  activateButtons(document.querySelector('.nav-button'));
+  loadHome();
+}
+
+export default loadWebsite;
